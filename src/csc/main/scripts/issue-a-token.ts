f9cdd87672320b5f86906f64d0146f2a88873e0e
morgan bergen <morgan@ku.edu>
2023-09-20 17:54:55 -0500

    main provided

--- src/csc/main/scripts/issue-a-token.ts
+++ src/csc/main/scripts/issue-a-token.ts
@@ -1,11 +1,14 @@
 import { Client, Wallet } from 'xrpl';
 
-async function main() {
+async function connect() {
     const client = new Client('wss://s.altnet.rippletest.net:51233');
     console.log("Connecting to Testnet...");
     await client.connect();
 
     client.disconnect();
+}
+function main() {
+    connect();
 }
 
 main();
