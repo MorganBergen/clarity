de4e746c2d63370b893ed95ca67bc83f1c37db74
morgan bergen <morganmahabergen@icloud.com>
2024-09-26 18:04:00 -0500

    successfully submitting questions should redirect to the following

---
+++ client/src/components/MainDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainDashboard = () => {
+  return (
+    <div>
+      <h1>Hello, this is the main dashboard</h1>
+    </div>
+  );
+};
+
+export default MainDashboard;
