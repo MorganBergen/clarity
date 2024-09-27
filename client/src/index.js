a3d1232aa046df9598a1a9113cb4221a3f3ca97b
morgan bergen <morganmahabergen@icloud.com>
2024-09-27 18:07:46 -0500

    supressed the specific warning

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,15 @@ import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Suppress specific warning about ReactDOM.render
+const originalConsoleError = console.error;
+console.error = (...args) => {
+  if (args[0].includes('ReactDOM.render is no longer supported in React 18')) {
+    return;
+  }
+  originalConsoleError(...args);
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
