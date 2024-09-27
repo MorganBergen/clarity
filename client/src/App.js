b4f79dda18500b3c21290d4ef50db51d12c27cea
morgan bergen <morganmahabergen@icloud.com>
2024-09-27 17:53:46 -0500

    added maindashboard path

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Dashboard from './components/Dashboard';
+import MainDashboard from './components/MainDashboard';
 import Login from './components/Login';
 import Register from './components/Register';
 import ForgotPassword from './components/ForgotPassword';
@@ -17,6 +18,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/MainDashboard" element={<MainDashboard />} />
           <Route path="/_/#/auth/confirm-verification/:token" element={<EmailVerification />} />
         </Routes>
       </Router>
