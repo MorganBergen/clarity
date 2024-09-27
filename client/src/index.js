7ac16d97050ac1355cf462a3becdf1fb21728ae6
morgan bergen <morganmahabergen@icloud.com>
2024-09-27 17:08:15 -0500

    strange formatting error

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
