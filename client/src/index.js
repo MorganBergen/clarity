8c156fc1fbbe51a6a7fa71cdfd7135351b33b142
morgan bergen <morganmahabergen@icloud.com>
2024-09-27 17:03:17 -0500

    created root using reactDOM create root method and rendered app

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
