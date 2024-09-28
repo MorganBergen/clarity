5d74a3d6856ded6e99f9abca42a34eddf88fdc71
morgan bergen <morganmahabergen@icloud.com>
2024-09-27 19:19:39 -0500

    mimic the main dashboard template

--- client/src/components/Analysis.js
+++ client/src/components/Analysis.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Box, Drawer, List, ListItem, ListItemText, Typography } from '@mui/material';
-import './Analysis.css';
+import './MainDashboard.css';
 
 const Analysis = () => {
+  const mainItems = [
+    { text: 'Dashboard' },
+    { text: 'Analysis' },
+    { text: 'Logging' },
+    { text: 'Reports' },
+  ];
+
+  const bottomItems = [
+    { text: 'Settings' },
+    { text: 'Sign Out', link: '/'},
+  ];
+
   return (
     <Box sx={{ display: 'flex' }}>
       <Drawer
@@ -14,20 +26,30 @@ const Analysis = () => {
           [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
         }}
       >
-        <List>
-          {['Dashboard', 'Analysis', 'Logging', 'Reports', 'Settings', 'Sign Out'].map((text, index) => (
-            <ListItem button key={text} component={Link} to={`/${text === 'Dashboard' ? 'MainDashboard' : text.toLowerCase().replace(' ', '-')}`}>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+        <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+          <List className="main-list">
+            {mainItems.map(({ text }) => (
+              <ListItem button key={text} component={Link} to={`/${text === 'Dashboard' ? 'MainDashboard' : text.toLowerCase().replace(' ', '-')}`}>
+                <ListItemText primary={text} className="page-text-color" />
+              </ListItem>
+            ))}
+          </List>
+          <div className="separator-line"></div>
+          <List className="bottom-list">
+            {bottomItems.map(({ text }) => (
+              <ListItem button key={text} component={Link} to={`/${ text === 'Sign Out' ? '' : text.toLowerCase().replace(' ', '-')}`}>
+                <ListItemText primary={text} className="page-text-color" />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Drawer>
       <Box
         component="main"
         sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
       >
         <Typography variant="h4">Analysis</Typography>
-        <Typography variant="body1">Nutrient Distribution Summary</Typography>
+        <Typography variant="body1">Detailed Nutrient Distribution</Typography>
       </Box>
     </Box>
   );
