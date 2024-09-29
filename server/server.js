7fc4df4f0ad8711d8d497acb06ce5aa81ff539aa
morgan bergen <morganmahabergen@icloud.com>
2024-09-28 20:05:34 -0500

    commented mongodb info - user, pass, connection string

--- server/server.js
+++ server/server.js
@@ -3,9 +3,23 @@
  *                  api endpoints where you can define routes or endpoints to handle requests
  */
 
-const app = require('./app');
+/**
+ * initial connection ip address 216.147.125.175/32 
+ * local connection 104.28.50.43/32
+ * username clarityfounders
+ * password nv2lYt5MLU1QYziI
+ * connection string mongodb+srv://clarityfounders:<db_password>@clarity.9saaa.mongodb.net/?retryWrites=true&w=majority&appName=Clarity
+ */
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`);
+});  
+
+app.use(bodyParser.json());
