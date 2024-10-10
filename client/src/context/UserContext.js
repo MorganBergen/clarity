edf3d959924f8b5cf0577b758492effb942a68ec
morgan bergen <morganmahabergen@icloud.com>
2024-10-10 18:51:59 -0500

    added use effect for local storage option

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,9 +1,20 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [userId, setUserId] = useState(null);
+    
+    const [userId, setUserId] = useState(() => {
+        return localStorage.getItem('userId');
+    });
+
+    useEffect(() => {
+        if (userId) {
+            localStorage.setItem('userId', userId);
+        } else {
+            localStorage.removeItem('userId');
+        }
+    }, [userId]);
 
     return (
         <UserContext.Provider value={{ userId, setUserId }}>
