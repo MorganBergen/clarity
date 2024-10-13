faa2404e72e304ee30115cda878c988ede0e0454
morgan bergen <morganmahabergen@icloud.com>
2024-10-13 16:09:16 -0500

    services directory containing the analyzeImage functionaity

---
+++ client/src/services/clarifaiService.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const PAT = '6bd0b7c74ee84bcc9d3b8219fc1f4865';
+const USER_ID = 'clarifai';
+const APP_ID = 'main';
+const MODEL_ID = 'food-item-recognition';
+const MODEL_VERSION_ID = '1d5fd481e0cf4826aa72ec3ff049e044';
+
+export const analyzeImage = async (imageUrl) => {
+    const raw = JSON.stringify({
+
+    });
+
+    const requestOption = {
+
+    };
+
+    try {
+
+    } catch (error) {
+
+    } 
+};
