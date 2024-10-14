51940e02d03195c419e47c2e0e2c5fdb8e5623a8
morgan bergen <morganmahabergen@icloud.com>
2024-10-14 17:38:47 -0500

    updated the front end

--- client/src/components/Logging.js
+++ client/src/components/Logging.js
@@ -5,13 +5,15 @@ import { AttachFile, CloudUpload } from '@mui/icons-material';
 import './MainDashboard.css';
 import PocketBase from 'pocketbase';
 import { UserContext } from '../context/UserContext';
-import axios from 'axios';
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 
 const Logging = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [imageUrl, setImageUrl] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+  const [uploadSuccess, setUploadSuccess] = useState(false);
+  const [showFileOptions, setShowFileOptions] = useState(false);
   const { userId } = useContext(UserContext);
 
   const mainItems = [
@@ -27,7 +29,9 @@ const Logging = () => {
   ];
 
   const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
+    const file = event.target.files[0];
+    setSelectedFile(file);
+    setPreviewUrl(URL.createObjectURL(file));
   };
 
   const handleUpload = async () => {
@@ -41,6 +45,7 @@ const Logging = () => {
       const response = await pb.collection('food').create(formData);
       console.log('Image uploaded successfully:', response);
       fetchImage(response.id);
+      setUploadSuccess(true);
     } catch (error) {
       console.error('Error uploading image:', error);
     }
@@ -58,61 +63,76 @@ const Logging = () => {
   };
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Drawer
-        variant="permanent"
-        sx={{
-          width: 240,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
-        }}
-      >
-        <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-          <List className="main-list">
-            {mainItems.map(({ text }) => (
-              <ListItem button={text.toString()} key={text} component={Link} to={`/${text === 'Dashboard' ? 'MainDashboard' : text.toLowerCase().replace(' ', '-')}`}>
-                <ListItemText primary={text} className="page-text-color" />
-              </ListItem>
-            ))}
-          </List>
-          <div className="separator-line"></div>
-          <List className="bottom-list">
-            {bottomItems.map(({ text }) => (
-              <ListItem button={text.toString()} key={text} component={Link} to={`/${text === 'Sign Out' ? '' : text.toLowerCase().replace(' ', '-')}`}>
-                <ListItemText primary={text} className="page-text-color" />
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      </Drawer>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}
-      >
-        <Typography variant="h4" sx={{ mb: 2 }}>Logging</Typography>
-        <Typography variant="body1" sx={{ mb: 4 }}>Upload images, or upload scanned barcode of your food</Typography>
-        <Box sx={{ display: 'flex', alignItems: 'center', mb: 4 }}>
-          <input
-            type="file"
-            id="file-input"
-            style={{ display: 'none' }}
-            onChange={handleFileChange}
-          />
-          <label htmlFor="file-input">
-            <IconButton color="primary" component="span">
-              <AttachFile />
-            </IconButton>
-          </label>
-          <IconButton color="primary" onClick={handleUpload} disabled={!selectedFile}>
-            <CloudUpload />
-          </IconButton>
-        </Box>
-        {imageUrl && (
-          <Box sx={{ mt: 3, textAlign: 'center' }}>
-            <Typography variant="h6">Uploaded Image:</Typography>
-            <img src={imageUrl} alt="Uploaded" style={{ maxWidth: '200px', height: 'auto', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }} />
+    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      <Box sx={{ display: 'flex' }}>
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: 240,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
+          }}
+        >
+          <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+            <List className="main-list">
+              {mainItems.map(({ text }) => (
+                <ListItem button={text.toString()} key={text} component={Link} to={`/${text === 'Dashboard' ? 'MainDashboard' : text.toLowerCase().replace(' ', '-')}`}>
+                  <ListItemText primary={text} className="page-text-color" />
+                </ListItem>
+              ))}
+            </List>
+            <div className="separator-line"></div>
+            <List className="bottom-list">
+              {bottomItems.map(({ text }) => (
+                <ListItem button={text.toString()} key={text} component={Link} to={`/${text === 'Sign Out' ? '' : text.toLowerCase().replace(' ', '-')}`}>
+                  <ListItemText primary={text} className="page-text-color" />
+                </ListItem>
+              ))}
+            </List>
           </Box>
-        )}
+        </Drawer>
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'center' }}
+        >
+
+          <Typography variant="h4" sx={{ mb: 2, textAlign: 'left' }}>Logging</Typography>
+          <Typography variant="h6" sx={{ mb: 2, textAlign: 'left' }}>Upload Images or Scanned Barcode of Your Food</Typography>
+          <Button variant="contained" color="primary" onClick={() => setShowFileOptions(!showFileOptions)}>
+            Add Food
+          </Button>
+          {showFileOptions && (
+            <Box sx={{ display: 'flex', alignItems: 'center', mb: 4, mt: 2 }}>
+              <input
+                type="file"
+                id="file-input"
+                style={{ display: 'none' }}
+                onChange={handleFileChange}
+              />
+              <label htmlFor="file-input">
+                <IconButton color="primary" component="span">
+                  <AttachFile />
+                </IconButton>
+              </label>
+              <IconButton color="primary" onClick={handleUpload} disabled={!selectedFile}>
+                <CloudUpload />
+              </IconButton>
+            </Box>
+          )}
+          {previewUrl && (
+            <Box sx={{ mt: 3, textAlign: 'center' }}>
+              <Typography variant="h6">File Preview:</Typography>
+              <img src={previewUrl} alt="Preview" style={{ maxWidth: '200px', height: 'auto', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }} />
+            </Box>
+          )}
+          {imageUrl && (
+            <Box sx={{ mt: 3, textAlign: 'center' }}>
+              <Typography variant="h6">Uploaded Image:</Typography>
+              <img src={imageUrl} alt="Uploaded" style={{ maxWidth: '200px', height: 'auto', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }} />
+              {uploadSuccess && <Typography variant="body2" sx={{ mt: 2, color: 'green' }}>Upload successful!</Typography>}
+            </Box>
+          )}
+        </Box>
       </Box>
     </Box>
   );
