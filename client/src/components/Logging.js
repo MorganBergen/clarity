192b6baea9254a3ad4e540304c00acb7fa6fc103
morgan bergen <morganmahabergen@icloud.com>
2024-10-18 15:05:43 -0500

    altered Box main and added function

--- client/src/components/Logging.js
+++ client/src/components/Logging.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Box, Drawer, List, ListItem, ListItemText, Typography, IconButton } from '@mui/material';
 import { CloudUpload } from '@mui/icons-material'; //  AttachFile import
@@ -14,6 +14,9 @@ const Logging = () => {
   const [previewUrl, setPreviewUrl] = useState(null);
   const [uploadSuccess, setUploadSuccess] = useState(false);
   const { userId } = useContext(UserContext);
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+  const [currentDate, setCurrentDate] = useState(new Date().toLocaleDateString());
+  const [userImages, setUserImages] = useState([]);
 
   const mainItems = [
     { text: 'Dashboard' },
@@ -26,6 +29,34 @@ const Logging = () => {
     { text: 'Settings' },
     { text: 'Sign Out', link: '/' },
   ];
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+      setCurrentTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => {
+    if (userId) {
+      fetchUserImages();
+    }
+  }, [userId]);
+
+  const fetchUserImages = async () => {
+    try {
+      const records = await pb.collection('food').getFullList({
+        filter: `userId="${userId}"`,
+      });
+
+      const images = records.map(record => `http://127.0.0.1:8090/api/files/food/${record.id}/${record.item[0]}`);
+      setUserImages(images);
+
+    } catch (error) {
+      console.error('Error fetching user images:', error);
+    }
+  }
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -41,10 +72,14 @@ const Logging = () => {
     formData.append('userId', userId);
 
     try {
+
       const response = await pb.collection('food').create(formData);
       console.log('Image uploaded successfully:', response);
       fetchImage(response.id);
       setUploadSuccess(true);
+      fetchUserImages();
+      setPreviewUrl(null)
+
     } catch (error) {
       console.error('Error uploading image:', error);
     }
@@ -89,40 +124,41 @@ const Logging = () => {
             </List>
           </Box>
         </Drawer>
-        <Box
-          component="main"
-          sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'center' }}
-        >
-          <Typography variant="h4" sx={{ mb: 2, textAlign: 'left' }}>Logging</Typography>
-          <Typography variant="h6" sx={{ mb: 2, textAlign: 'left' }}>Upload Images or Scanned Barcode of Your Food</Typography>
-          <Box sx={{ display: 'flex', alignItems: 'center', mb: 4, mt: 2 }}>
-            <input
-              type="file"
-              id="file-input"
-              style={{ display: 'none' }}
-              onChange={handleFileChange}
-            />
+
+        <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+            <Typography variant="h4" sx={{ mb: 2, textAlign: 'left' }}>Logging</Typography>
+            <input type="file" id="file-input" style={{ display: 'none' }} onChange={handleFileChange} />
             <label htmlFor="file-input">
               <Button variant="contained" color="primary" component="span">
                 Add Food
               </Button>
             </label>
-            <IconButton color="primary" onClick={handleUpload} disabled={!selectedFile}>
-
-              <CloudUpload />
-            </IconButton>
           </Box>
-          {previewUrl && (
-            <Box sx={{ mt: 3, textAlign: 'center' }}>
-              <img src={previewUrl} alt="Preview" style={{ maxWidth: '100px', height: 'auto', borderRadius: '4px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }} />
+
+          <Typography variant="h6" sx={{ mb: 2, textAlign: 'left' }}>Upload Images or Scanned Barcode of Your Food</Typography>
+          <Typography variant="body1" sx={{ mb: 2, textAlign: 'left' }}>{currentDate} {currentTime}</Typography>
+
+          <Button variant="contained" color="primary" component="span" onClick={handleUpload} disabled={!selectedFile}>
+            Upload
+          </Button>
+
+
+          <Box sx={{ mt: 3 }}>
+          {previewUrl && (<img src={previewUrl} alt="Preview" className="preview-image" />)}
+
+            <Typography variant="h6">Your Uploaded Images:</Typography>
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2 }}>
+              {userImages.map((url, index) => (
+                <img
+                  key={index}
+                  src={url}
+                  alt={`Uploaded ${index}`}
+                  className="uploaded-image"
+                />
+              ))}
             </Box>
-          )}
-          {imageUrl && (
-            <Box sx={{ mt: 3, textAlign: 'center' }}>
-              <Typography variant="h6">Uploaded Image:</Typography>
-              {uploadSuccess && <Typography variant="body2" sx={{ mt: 2, color: 'green' }}>Upload successful!</Typography>}
-            </Box>
-          )}
+          </Box>
         </Box>
       </Box>
     </Box>
