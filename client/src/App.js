70bb98afd7b186f30fa33d3d03e3e5233c72173f
morgan bergen <morganmahabergen@icloud.com>
2024-10-20 23:11:27 -0500

    added new route and commented out unfit DashboardLayout

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from '@mui/material/styles';
 import store from './redux/store';
 import Dashboard from './components/Dashboard';
+import Interface from './components/dashboard/interface';
 import MainDashboard from './components/MainDashboard';
 import Login from './components/Login';
 import Register from './components/Register';
@@ -14,16 +14,21 @@ import Settings from './components/Settings';
 import Logging from './components/Logging';
 import Reports from './components/Reports';
 import { UserProvider } from './context/UserContext';
+// import DashboardIcon from '@mui/icons-material/Dashboard';
+// import AnalysisIcon from '@mui/icons-material/QueryStats';
+// import SettingsIcon from '@mui/icons-material/Settings';
+// import LoggingIcon from '@mui/icons-material/Today';
+// import ReportsIcon from '@mui/icons-material/FolderCopy';
 
-import { AppProvider } from '@toolpad/core';
-import { createTheme } from '@mui/material/styles';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import AnalysisIcon from '@mui/icons-material/QueryStats';
-import SettingsIcon from '@mui/icons-material/Settings';
-import LoggingIcon from '@mui/icons-material/Today';
-import ReportsIcon from '@mui/icons-material/FolderCopy';
-import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+// import { createTheme } from '@mui/material/styles';
+// import DashboardIcon from '@mui/icons-material/Dashboard';
+// import AnalysisIcon from '@mui/icons-material/QueryStats';
+// import SettingsIcon from '@mui/icons-material/Settings';
+// import LoggingIcon from '@mui/icons-material/Today';
+// import ReportsIcon from '@mui/icons-material/FolderCopy';
+// import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 
+/*
 const NAVIGATION = [
   {
     segment: 'MainDashboard',
@@ -63,50 +68,52 @@ const NAVIGATION = [
     kind: 'page',
   },
 ];
+*/
 
 
-const Theme = createTheme({
-  cssVariables: {
-    colorSchemeSelector: 'data-toolpad-color-scheme',
-  },
-  colorSchemes: { light: true, dark: true },
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 600,
-      lg: 1200,
-      xl: 1536,
-    },
-  },
-});
+// const Theme = createTheme({
+//   cssVariables: {
+//     colorSchemeSelector: 'data-toolpad-color-scheme',
+//   },
+//   colorSchemes: { light: true, dark: true },
+//   breakpoints: {
+//     values: {
+//       xs: 0,
+//       sm: 600,
+//       md: 600,
+//       lg: 1200,
+//       xl: 1536,
+//     },
+//   },
+// });
 
 function App() {
   return (
     <Provider store={store}>
-      <AppProvider navigation={NAVIGATION} theme={Theme}>
-        <DashboardLayout>
+      {/* <AppProvider navigation={NAVIGATION} theme={Theme}> */}
+        {/* <DashboardLayout> */}
           <UserProvider>
             <Router>
               <Routes>
-                /* prior to login */
+                {/* prior to login */}
                 <Route path="/" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/_/#/auth/confirm-verification/:token" element={<EmailVerification />} />
 
-                /* after login */
+                {/* after login */}
                 <Route path="/MainDashboard" element={<MainDashboard />} />
                 <Route path="/Analysis" element={<Analysis />} />
                 <Route path="/Settings" element={<Settings />} />
                 <Route path="/Logging" element={<Logging />} />
                 <Route path="/Reports" element={<Reports />} />
+                <Route path="/Interface" element={<Interface />} />
               </Routes>
             </Router>
           </UserProvider>
-        </DashboardLayout>
-      </AppProvider>
+        {/* </DashboardLayout> */}
+      {/* </AppProvider> */}
     </Provider>
   );
 }
